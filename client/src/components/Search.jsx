6b7f9b46506c89285ac3f01aa67083b2f9348abc
Paul Diamond <pfdiamond13@gmail.com>
2019-06-12 14:38:14 -0700

    Implement Search Component

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { SearchInput } from 'evergreen-ui';
+
+const Search = ({ handleSearch }) => {
+  return <SearchInput onChange={handleSearch} marginRight={16} placeholder="Type to search..." width="100%" />;
+
+}
+
+export default Search;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Pane } from 'evergreen-ui';
+import Search from './components/Search';
+
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      searchValue: '',
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(e) {
+    this.setState({
+      searchValue: e.target.value,
+    });
   }
 
   render() {
-    return <div>Hello World</div>;
+    return (
+      <Pane>
+        <Pane
+          height={120}
+          width="100%"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          border="default"
+          background="tint2"
+        >
+          <Search handleSearch={this.handleSearch} />
+        </Pane>
+      </Pane>
+    );
   }
 }
 
