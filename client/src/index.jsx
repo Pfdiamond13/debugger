b772fd56ab88ae3ea6297cb70cd0d9f3ab908c7d
Paul Diamond <pfdiamond13@gmail.com>
2019-06-12 16:38:27 -0700

    Implement componentDidMount with initial GET request

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import { Pane, Button } from 'evergreen-ui';
 import Search from './components/Search';
 import Events from './components/Events';
@@ -10,12 +11,24 @@ class App extends React.Component {
     super();
     this.state = {
       searchValue: '',
-      isLive: false,
+      isLive: true,
       isPaused: false,
-      events: [{ type: "track", messageId: "ajs-67c7e4d7-1430-48ca-a716-5f6d1cfd85d7", context: { ip: "39.197.248.177", library: { name: "analytics.js", version: "3.0.0" } }, integrations: {}, receivedAt: "2019-06-12T04:00:06.295Z", sentAt: 1560312001295, userId: "592c2a67-18e1-407e-b542-e0b85c59345b", anonymousId: "b9961406-3e3d-4957-b9d2-acbf3c24a294", event: "Experiment Viewed" }, { type: "page", messageId: "ajs-8198cfa0-2991-4418-ab4e-21d8f6f8cec6", context: { ip: "177.233.251.132", library: { name: "analytics.js", version: "3.0.0" } }, integrations: {}, receivedAt: "2019-06-12T04:00:06.296Z", sentAt: 1560312001296, userId: "23c046cf-6955-490e-a4cb-6507bcffeafb", anonymousId: "12592b9c-0d12-413c-849d-393807d764d2", properties: { path: "/", "referrer": "", search: "", title: "ut accusantium ducimus", url: "https://alexa.name" } }]
+      events: [],
     };
     this.handleSearch = this.handleSearch.bind(this);
     this.handlePauseResume = this.handlePauseResume.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('/api/events')
+      .then(response => (
+        response.data
+      ))
+      .then((eventData) => {
+        this.setState({
+          events: eventData,
+        });
+      });
   }
 
   handleSearch(e) {
@@ -56,7 +69,7 @@ class App extends React.Component {
           <Button marginRight={16} isActive={isPaused} id="pause" onClick={this.handlePauseResume}>Pause</Button>
           <Search handleSearch={this.handleSearch} />
         </Pane>
-        {events.map(event => <Events event={event} />)}
+        {events.map((event, index) => <Events event={event} key={index} />)}
       </Pane>
     );
   }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
+const controllers = require('./controllers/events');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/public')));
+
+/************************* MiddleWare *******************************/
+
+/********************************************************************/
+
+/*************************   Routes   *******************************/
+
+app.get('/api/events', controllers.logEvents);
+
+/********************************************************************/
 
 app.listen(PORT, () => {
   console.log(`Listening on Port:${PORT}`);
