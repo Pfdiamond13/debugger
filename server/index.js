863836e885be886e254bd1217df3eaa27af76c81
Paul Diamond <pfdiamond13@gmail.com>
2019-06-13 00:53:30 -0700

    Implements socket.io and establishes connection to client

--- server/index.js
+++ server/index.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const path = require('path');
-const bodyparser = require('body-parser');
 
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const Redis = require('ioredis');
 
+// Intializes new ioredis instance
 const redis = new Redis();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../client/public')));
 
-/************************* MiddleWare *******************************/
-
-app.use(bodyparser.json());
-
-/********************************************************************/
-
 /*************************   Connections   *******************************/
 
-redis.subscribe('events', () => {
-  console.log('Redis: subscribed');
+// Establishes connection to redis channel
+// Currently uses ioredis default reconnect strategy - will try every 2 seconds up to 50 times
+redis.subscribe('events', (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Redis: Subscribed');
 });
 
+// Upon receiving a message from redis channel, io.emit will send message to client
 redis.on('message', (channel, message) => {
   io.emit('message', message);
 });
 
+// Establishes connection to client
 io.on('connection', () => {
   console.log('User is Connected');
 });
